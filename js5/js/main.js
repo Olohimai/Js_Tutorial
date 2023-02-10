dd75a8aea96855cc41500ae8ef241b72efc8d03f
olohimaiakalumhe <olohimaiakalumhe@gmail.com>
2023-02-10 23:54:09 +0000

    wip

---
+++ js5/js/main.js
@@ -0,0 +1,40 @@
+//math method and properties
+console.log(Math.PI);
+
+//Math.trunc removes all the deceimals in pi
+console.log(Math.trunc(Math.PI));
+
+//Math.round and Math.celi rounds the decimal value to the nearest integer
+console.log(Math.round(Math.PI)); // 3
+console.log(Math.round(3.55)); // 4
+
+//Math.floor rounds the decimal down
+console.log(Math.floor(Math.PI)); //3
+console.log(Math.floor(4.56)) // remains 4
+
+//Math.pow calculates a number to the power of its second number however it takes to arguments
+console.log(Math.pow(2, 6));
+
+//Math.min method returns the smallest number from 0 to more numbers like wise the Math.max method returns the largest number from 0 to more numbers
+
+//Math.min method
+console.log(Math.min(2, 3, 4, 5, 6, 7, 8, 9, 10)); // 2
+
+//Math.max method
+console.log(Math.max(1, 2, 3, 4, 5)); // 5
+
+//Math.random method returns random numbers from 0-1
+console.log(Math.random() * 100);
+console.log(Math.random() * 10);
+
+//Math.random method that returns a random number btw 0-9 how ever this can be done by adding a Math.floor method to round the decimal numbers cause off course Math.random random decimal numbers
+console.log(Math.floor(Math.random() * 10) + 201);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
