0a85e75f15e135290f172c126a5a987f49d8ffc9
olohimaiakalumhe <olohimaiakalumhe@gmail.com>
2023-02-11 15:33:21 +0000

    wip

---
+++ js6/js/main.js
@@ -0,0 +1,19 @@
+//FIRST CODING CHALLENGE
+
+//write a javascript code to write a random letter in your name
+console.log('Gerrard'.charAt(Math.floor(Math.random() * 6)))
+console.log('Gerrard'.charAt(Math.floor(Math.random() * 6)))
+console.log('Gerrard'.charAt(Math.floor(Math.random() * 6)))
+console.log('Gerrard'.charAt(Math.floor(Math.random() * 6)))
+console.log('Gerrard'.charAt(Math.floor(Math.random() * 6)))
+console.log('Gerrard'.charAt(Math.floor(Math.random() * 6)))
+console.log('Gerrard'.charAt(Math.floor(Math.random() * 6)))
+
+//write a javascript code to write a random letter in your all name
+const myName = 'GLORY';
+console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
+
